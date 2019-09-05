3bba7130aed2bbc217fc9df59ffc6c5c5a06b647
JoacoDiablito <38868883+JoacoDiablito@users.noreply.github.com>
2019-09-05 19:28:10 -0300

    Add files via upload
    Diseño móvil

---
+++ index.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Gerencia de Fiscalización - INCAA</title>
+
+<link href="Fisca_Style.css" rel="stylesheet" type="text/css" />
+<link href="menudesplegable.css" rel="stylesheet" type="text/css">
+</head>
+<body background="FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<table width="100%" align="center">
+<tr>
+  <td><? include("index_botonera_2017.html"); ?>
+</td>
+</tr>
+<tr>
+  <td align="center" valign="middle"><? include("index_contenido.html"); ?>
+</td>
+</tr>
+<tr>
+  <td><? include("index_footer.html"); ?>
+</td>
+</tr>
+  </table>
+</body>
+</html>
